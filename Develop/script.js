06417054ad7c6ba27839c6c135b6f17314135485
NhatWin <nhat0933@gmail.com>
2022-10-25 18:26:19 -0500

    Made password includes selected criteria

--- Develop/script.js
+++ Develop/script.js
@@ -92,6 +92,7 @@ const upperCasedCharacters = [
 ];
 
 let finArray = [];
+let passwordArray = [];
 
 function generatePassword() {
   // TODO: Write your code here
@@ -110,12 +111,12 @@ generateBtn.addEventListener("click", writePassword);
 
 // After clicking generate password
 function generatePassword() {
-  const length = prompt("How many characters do you want in your password?");
+  let length = prompt("How many characters do you want in your password?");
   if (length < 8 || length > 129 || length === null || isNaN(length) === true) {
     alert("Password must be between 8-128 characters");
     generatePassword();
     return
-  } 
+  }
 
   const lower = confirm("Do you want lower case letters in your password?");
   const upper = confirm("Do you want upper case letters in your password?");
@@ -125,24 +126,43 @@ function generatePassword() {
   // Password prompt check
   if (lower === true) {
     finArray = lowerCasedCharacters.concat(finArray);
+    passwordArray.push(lowerCasedCharacters[Math.floor(lowerCasedCharacters.length * Math.random())]);
+    length--;
    }
   if (upper === true) {
     finArray = upperCasedCharacters.concat(finArray);
+    passwordArray.push(upperCasedCharacters[Math.floor(upperCasedCharacters.length * Math.random())]);
+    length--;
   }
   if (number === true) {
     finArray = numericCharacters.concat(finArray);
+    passwordArray.push(numericCharacters[Math.floor(numericCharacters.length * Math.random())]);
+    length--;
   }
   if (unique === true) {
     finArray = specialCharacters.concat(finArray);
+    passwordArray.push(specialCharacters[Math.floor(specialCharacters.length * Math.random())]);
+    length--;
   }
+
   if (lower === false && upper === false && number === false && unique === false) {
     alert("Password can not be made with given information");
     generatePassword();
     return
   }
+  
   //Password generator
-  for (let i = 0; i < length; i++) {
-    const passwordOutput = Array.from({ length }, () => finArray[Math.floor(finArray.length * Math.random())]).join("");
-  return passwordOutput;
-  }
+    const passwordOutput = Array.from({ length }, () => finArray[Math.floor(finArray.length * Math.random())]).concat(passwordArray).join("");
+    let shuffle = [...passwordOutput];
+  
+  //Fisher-Yates shuffle algorithm 
+    const getRandomValue = (i, N) => Math.floor(Math.random() * (N - i) + i);
+
+    shuffle.forEach( (elem, i, arr, j = getRandomValue(i, arr.length)) => [arr[i], arr[j]] = [arr[j], arr[i]] );
+
+    shuffle = shuffle.join('');
+
+    console.log(shuffle);
+    return shuffle
+
 }
